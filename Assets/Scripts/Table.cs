4c9bb2802a987130273e75caa35387cefe313a3b
Irma <frank.irma@hotmail.de>
2019-01-09 15:05:22 +0100

    1

--- Assets/Scripts/Table.cs
+++ Assets/Scripts/Table.cs
@@ -25,100 +25,100 @@ public class Table : MonoBehaviour
     private eStatisfaction mStatisfaction;
     private Player mPlayer;
 
-    private void Start()
-    {
-        mSprite = pPanel.transform.GetChild(0).GetComponent<Image>();
-        mPlayer = FindObjectOfType<Player>();
+    //private void Start()
+    //{
+    //    mSprite = pPanel.transform.GetChild(0).GetComponent<Image>();
+    //    mPlayer = FindObjectOfType<Player>();
 
-        pState = eTableState.Idle;
-        mNextOrder = GameManager.pInstance.pRandom.Next(pEarliestFirstOrder, pLatestFirstOrder);
-        transform.GetChild(0).gameObject.SetActive(false);
-    }
+    //    pState = eTableState.Idle;
+    //    mNextOrder = GameManager.pInstance.pRandom.Next(pEarliestFirstOrder, pLatestFirstOrder);
+    //    transform.GetChild(0).gameObject.SetActive(false);
+    //}
 
-    private void Update()
-    {
-        switch (pState)
-        {
-            case eTableState.Idle:
-                if (Time.timeSinceLevelLoad >= mNextOrder)
-                {
-                    mTimestampWaiting = Time.timeSinceLevelLoad;
-                    pDesire = (eDishes)(GameManager.pInstance.pRandom.Next(2) + 1);
-                    mStatisfaction = eStatisfaction.Good;
-                    pState = eTableState.WaitingForOrder;
-                    mSprite.sprite = GameManager.pInstance.pEmotionSprites[0];
-                    transform.GetChild(0).gameObject.SetActive(true);
+    //private void Update()
+    //{
+    //    switch (pState)
+    //    {
+    //        case eTableState.Idle:
+    //            if (Time.timeSinceLevelLoad >= mNextOrder)
+    //            {
+    //                mTimestampWaiting = Time.timeSinceLevelLoad;
+    //                pDesire = (eDishes)(GameManager.pInstance.pRandom.Next(2) + 1);
+    //                mStatisfaction = eStatisfaction.Good;
+    //                pState = eTableState.WaitingForOrder;
+    //                mSprite.sprite = GameManager.pInstance.pEmotionSprites[0];
+    //                transform.GetChild(0).gameObject.SetActive(true);
 
-                }
-                break;
-            case eTableState.WaitingForOrder:
-                if (Time.timeSinceLevelLoad - mTimestampWaiting > pPatienceTime)
-                {
-                    mTimestampWaiting = Time.timeSinceLevelLoad;
-                    if (mStatisfaction == eStatisfaction.Angry)
-                    {
-                        mNextOrder = (int)Time.timeSinceLevelLoad + GameManager.pInstance.pRandom.Next(pMinTimeNextOrder, pMaxTimeNextOrder);
-                        transform.GetChild(0).gameObject.SetActive(false);
-                        pState = eTableState.Idle;
-                    }
-                    else
-                    {
-                        mStatisfaction++;
-                        mSprite.sprite = GameManager.pInstance.pEmotionSprites[(int)mStatisfaction];
-                    }
-                }
-                break;
-            case eTableState.Orders:
-                mSprite.sprite = GameManager.pInstance.pDisheSprites[(int)pDesire - 1];
-                mTimestampWaiting = Time.timeSinceLevelLoad + 5;
-                break;
-            case eTableState.WaitingForFood:
-                if (Time.timeSinceLevelLoad - mTimestampWaiting > pPatienceTime)
-                {
-                    mTimestampWaiting = Time.timeSinceLevelLoad;
-                    if (mStatisfaction == eStatisfaction.Angry)
-                    {
-                        mNextOrder = (int)Time.timeSinceLevelLoad + GameManager.pInstance.pRandom.Next(pMinTimeNextOrder, pMaxTimeNextOrder);
-                        transform.GetChild(0).gameObject.SetActive(false);
-                        pState = eTableState.Idle;
-                    }
-                    else
-                    {
-                        mStatisfaction++;
-                        mSprite.sprite = GameManager.pInstance.pEmotionSprites[(int)mStatisfaction];
-                    }
-                }
-                break;
-            case eTableState.Eats:
-                if (Time.timeSinceLevelLoad >= mTimestampWaiting)
-                {
-                    pState = eTableState.WaitingForClean;
-                    mTimestampWaiting = Time.timeSinceLevelLoad;
-                    mSprite.sprite = GameManager.pInstance.pDollarSprites[0];
-                    transform.GetChild(0).gameObject.SetActive(true);
-                }
-                break;
-            case eTableState.WaitingForClean:
-                if (Time.timeSinceLevelLoad - mTimestampWaiting > pPatienceTime)
-                {
-                    mTimestampWaiting = Time.timeSinceLevelLoad;
-                    if (mStatisfaction == eStatisfaction.Displeased)
-                    {
-                        mNextOrder = (int)Time.timeSinceLevelLoad + GameManager.pInstance.pRandom.Next(pMinTimeNextOrder, pMaxTimeNextOrder);
-                        transform.GetChild(0).gameObject.SetActive(false);
-                        pState = eTableState.Idle;
-                    }
-                    else
-                    {
-                        mStatisfaction++;
-                        mSprite.sprite = GameManager.pInstance.pDollarSprites[(int)mStatisfaction];
-                    }
-                }
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-    }
+    //            }
+    //            break;
+    //        case eTableState.WaitingForOrder:
+    //            if (Time.timeSinceLevelLoad - mTimestampWaiting > pPatienceTime)
+    //            {
+    //                mTimestampWaiting = Time.timeSinceLevelLoad;
+    //                if (mStatisfaction == eStatisfaction.Angry)
+    //                {
+    //                    mNextOrder = (int)Time.timeSinceLevelLoad + GameManager.pInstance.pRandom.Next(pMinTimeNextOrder, pMaxTimeNextOrder);
+    //                    transform.GetChild(0).gameObject.SetActive(false);
+    //                    pState = eTableState.Idle;
+    //                }
+    //                else
+    //                {
+    //                    mStatisfaction++;
+    //                    mSprite.sprite = GameManager.pInstance.pEmotionSprites[(int)mStatisfaction];
+    //                }
+    //            }
+    //            break;
+    //        case eTableState.Orders:
+    //            mSprite.sprite = GameManager.pInstance.pDisheSprites[(int)pDesire - 1];
+    //            mTimestampWaiting = Time.timeSinceLevelLoad + 5;
+    //            break;
+    //        case eTableState.WaitingForFood:
+    //            if (Time.timeSinceLevelLoad - mTimestampWaiting > pPatienceTime)
+    //            {
+    //                mTimestampWaiting = Time.timeSinceLevelLoad;
+    //                if (mStatisfaction == eStatisfaction.Angry)
+    //                {
+    //                    mNextOrder = (int)Time.timeSinceLevelLoad + GameManager.pInstance.pRandom.Next(pMinTimeNextOrder, pMaxTimeNextOrder);
+    //                    transform.GetChild(0).gameObject.SetActive(false);
+    //                    pState = eTableState.Idle;
+    //                }
+    //                else
+    //                {
+    //                    mStatisfaction++;
+    //                    mSprite.sprite = GameManager.pInstance.pEmotionSprites[(int)mStatisfaction];
+    //                }
+    //            }
+    //            break;
+    //        case eTableState.Eats:
+    //            if (Time.timeSinceLevelLoad >= mTimestampWaiting)
+    //            {
+    //                pState = eTableState.WaitingForClean;
+    //                mTimestampWaiting = Time.timeSinceLevelLoad;
+    //                mSprite.sprite = GameManager.pInstance.pDollarSprites[0];
+    //                transform.GetChild(0).gameObject.SetActive(true);
+    //            }
+    //            break;
+    //        case eTableState.WaitingForClean:
+    //            if (Time.timeSinceLevelLoad - mTimestampWaiting > pPatienceTime)
+    //            {
+    //                mTimestampWaiting = Time.timeSinceLevelLoad;
+    //                if (mStatisfaction == eStatisfaction.Displeased)
+    //                {
+    //                    mNextOrder = (int)Time.timeSinceLevelLoad + GameManager.pInstance.pRandom.Next(pMinTimeNextOrder, pMaxTimeNextOrder);
+    //                    transform.GetChild(0).gameObject.SetActive(false);
+    //                    pState = eTableState.Idle;
+    //                }
+    //                else
+    //                {
+    //                    mStatisfaction++;
+    //                    mSprite.sprite = GameManager.pInstance.pDollarSprites[(int)mStatisfaction];
+    //                }
+    //            }
+    //            break;
+    //        default:
+    //            throw new ArgumentOutOfRangeException();
+    //    }
+    //}
 
     public void StartEating()
     {
